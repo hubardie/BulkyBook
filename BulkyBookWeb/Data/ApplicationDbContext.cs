38aa2e52aa3b49ff0115380550669d851588f965
Hugo BD <hubardie@gmail.com>
2026-09-03 06:47:42 +0200

    Until lesson 32

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories        {            get; set;        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 3 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2},
+                new Category { Id = 3, Name = "History", DisplayOrder = 1 }
+            );
+        }
+
+    }
+}
--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,11 +1,14 @@
-﻿using System.Diagnostics.Contracts;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
 
 namespace BulkyBookWeb.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public int DisplayOrder { get; set; }
 
 
     }

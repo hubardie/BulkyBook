971c908c1b82220f87649cb5ed9fbe5e3e58d122
Hugo BD <hubardie@gmail.com>
2026-09-08 21:04:13 +0200

    Hasta lección 41

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -39,5 +39,40 @@ namespace BulkyBookWeb.Controllers
             }
             return View();
         }
+
+        public IActionResult Update(int? id)
+        {
+            if (id == null && id == 0)
+            {
+                return NotFound();            
+            }
+
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (category == null)
+            { 
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] // Validates that the request comes from the webpage
+        [ActionName("Update")]
+        public IActionResult UpdatePOST(Category category)
+        {
+            if (!string.IsNullOrEmpty(category.Name) && _context.Categories.Any(c => c.Name.ToLower() == category.Name.ToLower() && c.Id != category.Id))
+            {
+                ModelState.AddModelError("", "Category name already exists");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }

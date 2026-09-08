cca55dbf224ac02ee621b11e5d869d0d76dc4c57
Hugo BD <hubardie@gmail.com>
2026-09-08 18:18:49 +0200

    Hasta lección 37

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers
@@ -19,6 +20,16 @@ namespace BulkyBookWeb.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken] // Validates that the request comes from the webpage
+        [ActionName("Create")]
+        public IActionResult CreatePOST(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }

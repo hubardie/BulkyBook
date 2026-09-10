a783e1d84b68679ac58f658ae499ad6476bca968
Hugo Barceló Díez <hbarcelod.externos@dragados.com>
2026-09-10 15:28:56 +0200

    Hasta 49

---
+++ BulkyBook.Business/IServices/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using BulkyBook.Models;
+
+namespace BulkyBook.Business.IServices
+{
+    public interface ICategoryService
+    { 
+        Task<Category?> GetCategoryByIdASync(int id);
+        Task<IEnumerable<Category>> GetAllcategoriesAsync();
+        Task<Category> CreateCategoryAsync(Category category);
+        Task<Category> UpdateCategoryAsync(Category category);
+        Task DeleteCategoryAsync(int id);
+
+    }
+
+}
---
+++ BulkyBook.Business/Services/CategoryService.cs
@@ -0,0 +1,33 @@
+﻿using BulkyBook.Models;
+
+namespace BulkyBook.Business.IServices
+{
+    public class CategoryService : ICategoryService
+    {
+        public Task<Category> CreateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCategoryAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Category>> GetAllcategoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category?> GetCategoryByIdASync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> UpdateCategoryAsync(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}

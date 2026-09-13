322c80ead4e7d189311294d346b39d1c263f8fa8
Hugo BD <hubardie@gmail.com>
2026-09-13 15:07:13 +0200

    L62

---
+++ BulkyBook.Business/IServices/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using BulkyBook.Models;
+
+namespace BulkyBook.Business.IServices
+{
+    public interface IProductService
+    { 
+        Task<Product?> GetProductByIdASync(int id);
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task<Product> CreateProductyAsync(Product product);
+        Task UpdateProductAsync(Product product);
+        Task DeleteProductAsync(int id);
+        Task<bool> IsProductTitleUniqueAsync(string name, int? ProductId = null);
+    }
+
+}
---
+++ BulkyBook.Business/Services/ProductService.cs
@@ -0,0 +1,65 @@
+﻿using BulkyBook.Business.IServices;
+using BulkyBook.DataAccess.Data;
+using BulkyBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook.Business.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly ApplicationDbContext _context;
+        public async Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+        public async Task<Product?> GetProductByIdASync(int id)
+        {
+            return await _context.Products.FindAsync(id); 
+        }
+        public ProductService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public  async Task<Product> CreateProductAsync(Product Product)
+        {
+            _context.Add(Product);
+            await _context.SaveChangesAsync();
+            return Product;
+        }
+
+        public async Task DeleteProductAsync(int id)
+        {
+            var Product = await _context.Products.FindAsync(id);
+            if (Product == null)
+            {
+                throw new KeyNotFoundException($"Product {id} not found");
+            }
+            _context.Remove(Product);
+            await _context.SaveChangesAsync();  
+        }
+
+       
+
+        public async Task UpdateProductAsync(Product Product)
+        {
+            _context.Products.Update(Product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> IsProductTitleUniqueAsync(string name, int? ProductId = null) 
+        {
+            if (ProductId.HasValue)
+            {
+                return !await _context.Products.AnyAsync(c => c.Title.ToLower() == name.ToLower() && c.Id != ProductId.Value);
+            }
+            else
+            {
+                return !await _context.Products.AnyAsync(c => c.Title.ToLower() == name.ToLower());
+            }   
+
+        }
+
+    }
+
+}
